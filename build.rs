20454bd50460e7270af0c1bb359fd55ef29d3358
Emilio Cobos Álvarez <emilio@crisal.io>
2019-10-02 21:10:44 +0200

    Try to replicate the potential issue using the same ubuntu gcc and such.

--- build.rs
+++ build.rs
@@ -1,6 +1,39 @@
+use std::process::Command;
+use std::path::{Path, PathBuf};
+
 fn main() {
+    /*
     cc::Build::new()
         .cpp(true)
         .file("src/t.cc")
         .compile("libt.a");
+    */
+
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let obj = PathBuf::from(&out_dir).join("t.o");
+    Command::new("/home/emilio/src/moz/ubuntu/gcc-8/usr/bin/gcc-8")
+        .arg("-m32")
+        .arg("-fPIE")
+        .arg("-Os")
+        .arg("-c")
+        .arg("-o")
+        .arg(&obj)
+        .arg("src/t.cc")
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    let lib = PathBuf::from(&out_dir).join("libt.a");
+    Command::new("ar")
+        .arg("rcs")
+        .arg(&lib)
+        .arg(&obj)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    println!("cargo:rustc-link-search={}", out_dir.to_string_lossy());
+    println!("cargo:rustc-link-lib=t");
 }
